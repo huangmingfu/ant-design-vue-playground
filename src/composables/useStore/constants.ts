25c7728621555cf1f7a25a4cd27629261c82d229
黄明富 <212149997@qq.com>
2025-01-18 17:12:34 +0800

    chore: 添加注释等

--- src/composables/useStore/constants.ts
+++ src/composables/useStore/constants.ts
@@ -1,6 +1,6 @@
 export const IS_DEV = import.meta.env.VITE_NODE_ENV === 'development';
-export const MAIN_FILE = 'src/PlaygroundMain.vue';
-export const APP_FILE = 'src/App.vue';
+export const MAIN_FILE = 'src/PlaygroundMain.vue';// template/main.vue 要挂载的文件
+export const APP_FILE = 'src/App.vue'; // template/welcome.vue 当前激活的文件
 export const ANTDV_FILE = 'src/ant-design-vue.js';
 export const IMPORT_MAP = 'import-map.json';
 export const LEGACY_IMPORT_MAP = 'src/import_map.json';
